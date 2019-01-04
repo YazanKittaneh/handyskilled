405079d6e02ae6b9f435ba7b0a9f396ffe7775b2
Asim Zaidi <asimzaidih@gmail.com>
2019-01-04 14:46:28 -0600

    signup :construction:

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require("../models/user");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+/* GET homeowner page. */
+router.get('/homeowner', function(req, res, next) {
+  res.render('homeowner');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res) => {
+  console.log("req.body:", req.body);
+  
+  const username = req.body.username;
+  // const email = req.body.email;
+  User.findOne({username}, "username").then(user => {
+      if(user) {
+          return res.status(401).send({ message: "Account with this username already exists" });
+      } else {
+          const user = new User(req.body);
+          // SendGrid.sendWelcomeEmail(user);
+          user.save().then((user) => {
+              const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+              // set the cookie when someone signs up and logs in
+              res.cookie('nToken', token, { maxAge: 600000, httpOnly: true });
+              res.redirect("/dashboard");
+          }).catch(err => {
+              console.log(err.message);
+              return res.status(400).send({ err: err });
+          });
+      } // end else
+  }).catch((err) => {
+      console.log(err);
+  });
 });
 
 module.exports = router;
